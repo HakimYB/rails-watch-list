6f6a8715b357eafa372e3853d4c5ce9be659a476
Hakim Benkacem <hbenkacem@yahoo.co.uk>
2023-03-09 16:00:53 +0400

    route, controller and views created

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,30 @@
+class ListsController < ApplicationController
+
+  def home
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+    @movies = Movie.all
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'lists#home'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :lists, only: [:index, :show, :new, :create ] do
+    resources :bookmarks, only: [:new, :create, :index, :destroy ]
+    root to: 'bookmarks#home'
+  end
 end
